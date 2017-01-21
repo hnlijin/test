4c32d1211967607a57534fc754c980c8f98e5de9
jin <dnahz2008@163.com>
2017-01-21 18:43:03 +0800

    web

--- game/server/web/app.js
+++ game/server/web/app.js
@@ -1,14 +1,14 @@
 var express = require('express');
 var path = require('path');
-// var favicon = require('serve-favicon');s
+var favicon = require('serve-favicon');
 // var logger = require('morgan');
-// var cookieParser = require('cookie-parser');
-// var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 // var busboy = require('connect-busboy');
 // var hbs = require('hbs');
-
+var ejs = require('ejs');
 var masterServer = null;
-
+var router = express.Router();
 var app = express();
 
 app.setMaster = function (server) {
@@ -16,23 +16,30 @@ app.setMaster = function (server) {
 };
 
 // view engine setup
-// app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'views'));
 // app.set('view engine', 'hbs');
+app.set('view engine', 'ejs');
 
 // uncomment to use a banner
 //app.locals.banner = '/img/banner.png';
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(favicon(__dirname + '/public/favicon.ico'));
 //app.use(logger('dev'));
 // app.use(busboy({limits: {fileSize: 512 * 1024}}));
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({extended: false}));
-// app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '..', '..', 'client')));
+console.log('kkk1');
+/* GET home page. */
+router.get('/', function(req, res) {
+console.log('kkk');
+  	res.render('index', { title: 'index' });
+});
 
 app.get('/game', function (req, res, next) {
-    res.redirect('/client/0');
+    res.redirect('./../../client/0');
 });
 
 // catch 404 and forward to error handler
